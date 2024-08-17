2fc8093346a6280c057a117471b971fabe27a5e3
Morgan Keith <morgan.a.keith92@gmail.com>
2024-08-17 10:52:46 -0500

    8/17/2024

--- Home.py
+++ Home.py
@@ -4,3 +4,18 @@ from PIL import Image
 
 import streamlit as st
 
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+# show image
+image = Image.open('logo.jpg')
+
+
+st.image(
+    image, 
+    caption='Image', 
+    width=400, 
+    channels='RGB')
+
+st.divider()
